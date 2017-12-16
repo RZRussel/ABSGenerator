0d3780cc9be900aba034c1d3ff7e9edddad14ff6
Russel <emkil.russel@gmail.com>
2017-12-16 23:35:17 +0300

    [new] parser tests added

--- core/prism/tests/test_parser.py
+++ core/prism/tests/test_parser.py
@@ -2,4 +2,37 @@ from unittest import TestCase
 from antlr4 import ParseTreeWalker, CommonTokenStream, FileStream
 from ..parser.PrismTemplateLexer import PrismTemplateLexer
 from ..parser.PrismTemplateParser import PrismTemplateParser
-from ..parser.listeners import PrismReplacementsGatherer
+from ..parser.listeners import PrismReplacementsGatherer, PrismErrorListener
+
+
+class TestTemplateParser(TestCase):
+
+    def test_create_tree(self):
+        input_stream = FileStream("resources/test_model.prism")
+        lexer = PrismTemplateLexer(input_stream)
+        stream = CommonTokenStream(lexer)
+        parser = PrismTemplateParser(stream)
+
+        error_listener = PrismErrorListener()
+        parser.removeErrorListeners()
+        parser.addErrorListener(error_listener)
+
+        parser.program()
+
+        self.assertTrue(len(error_listener.msg_list) == 0)
+
+    def test_replacements_parsing(self):
+        input_stream = FileStream("resources/test_template.prism")
+        lexer = PrismTemplateLexer(input_stream)
+        stream = CommonTokenStream(lexer)
+        parser = PrismTemplateParser(stream)
+        tree = parser.program()
+
+        listener = PrismReplacementsGatherer()
+        walker = ParseTreeWalker()
+        walker.walk(listener, tree)
+
+        self.assertTrue("move" in listener.replacements)
+        self.assertTrue("max_x" in listener.replacements)
+        self.assertTrue("max_y" in listener.replacements)
+        self.assertTrue("max_direction" in listener.replacements)
